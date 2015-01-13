fbbfc09743f956394f17ab271e70e9a1dc11df8c
Devin Pastoor <devin.pastoor@gmail.com>
2015-01-13 16:39:24 -0500

    add some comments

--- connect_mongo.py
+++ connect_mongo.py
@@ -1,8 +1,11 @@
 import pymongo
 from pymongo import MongoClient
 connection = MongoClient('localhost', 3001)
+# connect via database name
 db = connection.meteor
+# get collection from database
 tasks = db.tasks
+# insert something into the collection
 tasks.insert({"text":'hello from python2'})
 #print(tasks.find_one()['text'])
 
